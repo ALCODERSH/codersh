abc94e48badbd0cdc5cb20085fd84d56a0a19d51
Govind R Nair <govindrnair.rev@ug.cusat.ac.in>
2023-04-25 21:03:15 +0530

    added value passing to html file in views

--- main/views.py
+++ main/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Language
 # Create your views here.
 def home_page(response):
     return render (response, 'main/pages/home.html')
 
 def editor_page(response):
-    return render (response, 'main/pages/editor.html')
+    lang = Language.objects.all()
+    return render (response, 'main/pages/editor.html', {"lang":lang})
