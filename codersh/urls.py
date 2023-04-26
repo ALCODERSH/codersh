a9edc13b7d63977f5abcfc61ceaee04bb040b329
Govind R Nair <govindrnair.rev@ug.cusat.ac.in>
2023-04-26 16:34:57 +0530

    new register app for user data and dummy signup page

--- codersh/urls.py
+++ codersh/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from register import views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('signup/', v.signup_page, name = "signup"),
     path('',include('main.urls')),
 ]
